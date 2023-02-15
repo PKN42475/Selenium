c8e896949890267d1ded1032745e964cb232dc9b
PKN42475 <rolexpavan3@gmail.com>
2023-02-15 10:54:55 +0530

    Selenium Complete Framework

---
+++ Selenium/src/test/java/TestCases/Flipkart.java
@@ -0,0 +1,23 @@
+package TestCases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Flipkart {
+
+	static {
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	}
+
+	@Test
+	public void Execution() {
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.flipkart.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	}
+}
